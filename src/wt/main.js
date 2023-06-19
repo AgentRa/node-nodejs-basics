cdd1524f205bfb89fdf9e7d7a1e3d33343fab2f9
agentRa <bubonik.san1993@gmail.com>
2023-06-19 21:40:05 +0200

    Fix: create proper number of workers based on number of cpus

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,11 +1,12 @@
 import { Worker } from "node:worker_threads";
 import { createAbsolutePath } from "../utils/createAbsolutePath.js";
+import { cpus } from "node:os";
 
 const performCalculations = async () => {
   const workerPromises = [];
   const source = createAbsolutePath(import.meta.url, "worker.js");
 
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < cpus().length; i++) {
     const workerPromise = new Promise((resolve, reject) => {
       const worker = new Worker(source, { workerData: i + 10 });
       worker.on("error", (error) => reject(error));
